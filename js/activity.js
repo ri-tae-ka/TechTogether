6031871594b71f15029ef30f71479286e30b5eac
K B Debowska <kbdebowska1@sheffield.ac.uk>
2021-10-16 23:46:03 -0400

    update

--- js/activity.js
+++ js/activity.js
@@ -1,3 +1,11 @@
+var state = {
+	selected_tags:new Array(),
+	supply:true,
+	time:30,
+	searchValue:'',
+	activities:null
+};
+
 // api url
 const api_url = 
       "https://employeedetails.free.beeceptor.com/my/api/path";
@@ -6,28 +14,26 @@ const api_url =
 async function getapi(url) {
     // Storing response
     const response = await fetch('http://localhost:3000/activities');
-    
     // Storing data in form of JSON
     var data = await response.json();
-	console.log(data)
-    console.log(typeof(data))
+    state.activities = data;
     show(data);
 }
 
 // Function to define innerHTML for HTML table
-function show(data) {
+function show() {
     let tab='';
     
     // Loop to access all rows 
-    for (let row in data) {
-        tab += `<div class="card" style="background-color:white;"><div class="card-body"><h5 style="font-weight:bold; text-align:center;" class="card-title">${data[row].Name}</h5>
+    for (let row in state.activities) {
+        tab += `<div class="card" style="background-color:white;"><div class="card-body"><h5 style="font-weight:bold; text-align:center;" class="card-title">${state.activities[row].Name}</h5>
 		<div class='parent'>
 			<div class='children' style="flex: 2;">
 				<button style="font-weight:bold;" class='btnTag'>VIRTUAL</button>
-				<p><span style='font-weight:bold;'>Time:</span> 30 min<br/><span style='font-weight:bold;'>Supply:</span>${data[row].Supply_tag}</p>
+				<p><span style='font-weight:bold;'>Time:</span> 30 min<br/><span style='font-weight:bold;'>Supply:</span>${state.activities[row].Supply_tag}</p>
 			</div>
 			<div class='children' style="flex: 4;">
-				<p class="card-text">${data[row].Description}</p>
+				<p class="card-text">${state.activities[row].Description}</p>
 			</div>
 		</div>
 	  </div>
@@ -37,18 +43,15 @@ function show(data) {
     document.getElementById("results").innerHTML = tab;
 }
 
-var state = {
-	selected_tags:new Array(),
-	supply:true,
-	time:30,
-	searchValue:''
-};
-
 function selectDefaultEvents(){
 	// Calling that async function
 	getapi(api_url);
 	document.getElementById("supply").click();
 	document.getElementById('30').click();
+}
+
+async function getOriginalData(){
+	await getapi(api_url);
 }
 
 function changeTag(e) {
@@ -83,6 +86,7 @@ function changeSupply(e) {
 		document.getElementById('supply').style = "background-color: white !important;";
 	}
 	console.log(state);
+	updateResults('supply');
 }
 
 function changeTime(e) {
@@ -94,13 +98,65 @@ function changeTime(e) {
 	document.getElementById(value).style = "background-color:orange !important;";
 	state.time = parseInt(value,10);
 	console.log(state);
+	updateResults('time');
 }
 
-function clickSearch(){
-	searchValue = document.getElementById('searchInput').value;
-	window.scroll({
-	  top: 100,
-	  left: 100,
-	  behavior: 'smooth'
-	});
+function deleteEntries() {
+    // Setting innerHTML as tab variable
+    document.getElementById("results").innerHTML = "";
+	console.log('delete')
 }
+
+async function updateResults(typeSearch){
+	await getOriginalData();
+	if (typeSearch==='search'){
+		for (index in state.activities){
+			let name;
+			name = state.activities[index].Name;
+			if (name.toUpperCase().includes(state.searchValue.toUpperCase()))
+				continue;
+			else
+				delete state.activities[index]
+		}
+		await deleteEntries();
+		await show();
+	}
+	else if (typeSearch==='supply'){
+		for (index in state.activities){
+			let supply;
+			supply = state.activities[index].Supply_tag;
+			if (state.supply===false){
+				if (supply==='-')
+					continue;
+				else
+					delete state.activities[index]
+			}
+			else{
+				if (supply==='-')
+					delete state.activities[index]
+				else
+					continue;
+			}
+		}
+		await deleteEntries();
+		await show();
+	}
+}
+
+async function clickSearch(){
+	state.searchValue = document.getElementById('searchInput').value;
+	console.log(state.searchValue)
+	if (state.searchValue===''){
+		await deleteEntries();
+		getapi(api_url);
+	}
+	else{
+		window.scroll({
+		  top: 100,
+		  left: 100,
+		  behavior: 'smooth'
+		});
+		updateResults('search');
+	}
+}
+
