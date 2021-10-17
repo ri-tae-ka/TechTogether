223538d08eb8d04c26c26a7364ac9653c8ce6fda
K B Debowska <kbdebowska1@sheffield.ac.uk>
2021-10-16 20:33:40 -0400

    update

--- js/activity.js
+++ js/activity.js
@@ -1,3 +1,43 @@
+// api url
+const api_url = 
+      "https://employeedetails.free.beeceptor.com/my/api/path";
+  
+// Defining async function
+async function getapi(url) {
+    // Storing response
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+	console.log(data)
+    //data = JSON.parse(data);
+    //show(data);
+}
+
+// Function to define innerHTML for HTML table
+function show(data) {
+    let tab = `<div class="card"><div class="card-body">`;
+    
+    // Loop to access all rows 
+    for (let key in data) {
+		console.log(r);
+        tab += `<h5 style="font-weight:bold; text-align:center;" class="card-title">${data[key]}</h5>
+		<div class='parent'>
+			<div class='children' style="flex: 2;">
+				<button style="font-weight:bold;" class='btnTag'>VIRTUAL</button>
+				<p><span style='font-weight:bold;'>Time:</span> 30 min<br/><span style='font-weight:bold;'>Supply:</span> Pen</p>
+			</div>
+			<div class='children' style="flex: 4;">
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			</div>
+		</div>
+	  </div>
+	</div>`;
+    }
+    // Setting innerHTML as tab variable
+    document.getElementById("results").innerHTML = tab;
+}
+
 var state = {
 	selected_tags:new Array(),
 	supply:true,
@@ -6,6 +46,8 @@ var state = {
 };
 
 function selectDefaultEvents(){
+	// Calling that async function
+	getapi(api_url);
 	document.getElementById("supply").click();
 	document.getElementById('30').click();
 }
